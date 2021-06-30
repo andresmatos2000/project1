f89fca83eead6e643a619e1b0d89fa8c4d42a6f9
Andres Matos <andresmatos2000@gmail.com>
2021-06-30 14:11:07 -0400

    Added newline

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv) {
     for(unsigned int i = 0; i < tokenList.size(); i++){
         cout << tokenList[i]->To_String() << endl;
     }
-    cout << "Total Tokens = " + to_string(tokenList.size()) + "\n";
+    cout << "Total Tokens = " + to_string(tokenList.size()) << endl;
     delete lexer;
 
     return 0;
